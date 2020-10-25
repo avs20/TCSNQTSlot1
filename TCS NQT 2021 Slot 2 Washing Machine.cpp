7393516a692ee8f3acf19127d82f4adf6aad7732
Ashutosh Singh <dh_ashu@hotmail.com>
2020-10-25 08:36:16 +0530

    Update TCS NQT 2021 Slot 2 Washing Machine.cpp

--- TCS NQT 2021 Slot 2 Washing Machine.cpp
+++ TCS NQT 2021 Slot 2 Washing Machine.cpp
@@ -12,8 +12,13 @@ int main() {
   }else if (weight > 7000){
     cout <<"OVERLOADED!!!";
   }else{
-
-      if (weight < 2000){
+    
+      /*I forgot this in video but there is a case for 0 weight also */
+      
+      if (weight == 0 ){
+        cout << "Time Estimated : 0 minutes";
+      }
+      else if (weight < 2000){
         cout << "Time Estimated : 25 minutes";
       }else if (weight < 4001){
         cout << "Time Estimated : 35 minutes";
